d82ab78a3d1eda6bdbc4ba64b186380be014b0a8
hc75 <vagrant@kubemaster>
2022-05-24 06:48:47 +0000

    init

---
+++ user-service/src/main/java/com/hc/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,15 @@
+package com.hc.user.VO;
+
+import com.hc.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+
+    private User user;
+    private Department department;
+}
